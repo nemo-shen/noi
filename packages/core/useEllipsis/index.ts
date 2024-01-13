29b151c4cff93ecb580dd7a986f9c9289dd2c984
nemo shen <nemoshen94@gmail.com>
2024-01-13 11:05:39 +0800

    fix(useEllipsis): opt algo

--- packages/core/useEllipsis/index.ts
+++ packages/core/useEllipsis/index.ts
@@ -1,4 +1,4 @@
-import { watch, watchEffect, type Ref } from 'vue'
+import { ref, watch, watchEffect, type Ref } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 
 interface UseEllipsisOptions {
@@ -12,6 +12,60 @@ interface UseEllipsisReturn {
   state: 'expand' | 'collapse' // 当前状态
   toggle: Function // 切换状态
 }
+
+function test() {
+  const source =
+    '擁有正確的座右銘，可以幫助你改變心態，自信迎接新的一天，比前一天更好。座右銘能改變你的生活。將勵志名言納入內容，作為文案，用這些金句激勵讀者。讓名言成為你的引領，展翅高飛！'
+  const lineHeight = 16
+
+  const div = document.createElement('div')
+  // const width = div.style.setProperty('width', `${width}px`)
+  div.style.setProperty('line-height', `${lineHeight}px`)
+  document.body.appendChild(div)
+
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  /**
+   * 有一个空间，和一个字符串，请问最多能放多少个字符
+   * @param content
+   */
+  function calc(content: string, res = '', tail = '') {
+    if (content.length === 0) return
+    let half = ''
+    let remain = ''
+    if (content.length > 1) {
+      half = content.slice(0, content.length / 2)
+      remain = content.slice(half.length)
+
+      div.innerText = `${res}${half}`
+      if (div.clientHeight > lineHeight) {
+        // 此时说明half无法放入
+        div.innerText = `${res}`
+        calc(half, res, tail + remain)
+      } else {
+        // 此时说明half可以被正确放入
+        // TODO: 需要考虑下上一次的remain
+        calc(remain + tail, res + half, '')
+      }
+    } else {
+      half = content
+
+      div.innerText = `${res}${half}`
+      if (div.clientHeight > lineHeight) {
+        div.innerText = `${res}`
+      }
+    }
+    // TODO: 如果 content 只有一个字符，
+
+    // 如果一半的字符串能放进去，那么剩余的就是content / 2
+    // 如果一半的字符串不能放进去，那么下次取值应该是 content / 2 / 2
+    // 但是一半的一半能放进去，那么下次的取值应该是从剩余的部分继续取值
+  }
+
+  calc(source)
+}
+test()
+const fontSize = ref<number>(1)
+const end = ref<number>(0)
 const cloneElement = (target: HTMLElement): HTMLElement => {
   const originStyle = window.getComputedStyle(target)
   const container = document.createElement('div')
@@ -26,6 +80,10 @@ const cloneElement = (target: HTMLElement): HTMLElement => {
   container.style.height = 'auto'
   container.style.minHeight = 'auto'
   container.style.maxHeight = 'auto'
+  document.body.appendChild(container)
+
+  fontSize.value = parseFloat(container.style.getPropertyValue('font-size'))
+  end.value = container.clientWidth / fontSize.value
 
   return container
 }
@@ -45,7 +103,7 @@ export const useEllipsis = (
 
   const toggle = () => {}
   return {
-    content: options.content.slice(0, 10),
+    content: options.content.slice(0, 24),
     state: 'collapse',
     toggle,
   }
--- src/demo/UseEllipsis.vue
+++ src/demo/UseEllipsis.vue
@@ -4,11 +4,11 @@ import { useEllipsis } from '@noi/core'
 import { useWindowSize } from '@vueuse/core'
 const root = ref<HTMLElement>()
 const { content, state, toggle } = useEllipsis(root, {
-  content: '去拥抱陌生，去期待惊喜。向海风许愿，在山海相见。愿你保持善良，从此拥有远方。风停在窗边，嘱咐我要热爱这个世界。'
+  content: '擁有正確的座右銘，可以幫助你改變心態，自信迎接新的一天，比前一天更好。座右銘能改變你的生活。將勵志名言納入內容，作為文案，用這些金句激勵讀者。讓名言成為你的引領，展翅高飛！'
 })
 // const { width } = useWindowSize()
 </script>
 
 <template>
-  <div ref="root">{{ content }}</div>
+  <div ref="root"></div>
 </template>
