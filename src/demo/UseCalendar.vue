d25cf8980216a0139773f49b26f312359d94d815
nemo shen <nemoshen94@gmail.com>
2024-01-28 12:50:01 +0800

    chore: demo

--- src/demo/UseCalendar.vue
+++ src/demo/UseCalendar.vue
@@ -2,9 +2,14 @@
 import { ref, h, VNode } from 'vue'
 import { useCalendar } from '@noi/core'
 
-const { currentMonthDays } = useCalendar()
+const { currentMonthDays, goToNextMonth, goToPreviousMonth } = useCalendar()
 </script>
 
 <template>
+  <button @click="goToPreviousMonth">prev month</button>
+  <button @click="goToNextMonth">next month</button>
+  需要生成一个当前日历需要知道哪些信息 1. 当前的月份有多少天 2.
+  每天是一个对象，应该会配有不同的内容 3. 周 4. 当前的年月 5.
+
   {{ currentMonthDays }}
 </template>
