1431d4ca222a1befd3ee71ab07644a4cae59a107
Nemo <nemoshen94@gmail.com>
2024-02-16 17:23:05 +0800

    test: useUpload

---
+++ packages/core/useUpload/index.test.ts
@@ -0,0 +1,8 @@
+import { vi, describe, expect, test } from 'vitest'
+import { useUpload } from '.'
+
+describe('useEllipsis', () => {
+  test('should be defined', () => {
+    expect(useUpload).toBeDefined()
+  })
+})
