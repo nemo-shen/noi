d4ddf005cbc8afe8fd7e003c266213119602e4bd
Nemo <nemoshen94@gmail.com>
2024-02-16 17:30:35 +0800

    test: useRolling

---
+++ packages/core/useRolling/index.test.ts
@@ -0,0 +1,8 @@
+import { vi, describe, expect, test } from 'vitest'
+import { useRolling } from '.'
+
+describe('useRolling', () => {
+  test('should be defined', () => {
+    expect(useRolling).toBeDefined()
+  })
+})
