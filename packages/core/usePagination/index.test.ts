e0fd740d6bce8f085f696d723c1c69515bfa260c
nemo shen <nemoshen94@gmail.com>
2024-03-02 10:30:12 +0800

    fix(usePagiantion): init test

---
+++ packages/core/usePagination/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest'
+import { usePagination } from '.'
+
+describe('usePagination', () => {
+  test('should be defined', () => {
+    expect(usePagination).toBeDefined()
+  })
+})
